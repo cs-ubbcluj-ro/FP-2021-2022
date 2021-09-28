021e682d8db7fe5c84d1463cefd804a763f68fd1
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-09-28 08:42:09 +0300

    Wrote a bit more code for first lecture

--- src/lecture/livecoding/lecture_01.py
+++ src/lecture/livecoding/lecture_01.py
@@ -1,8 +1,39 @@
 print("Hello World!")
 
-value = input("Enter a value: ")
+# char value[10]; // int value;
 
-print(value)
+# static typing - each var's type is known at compile time
+# dynamic typing - Python interpreter keeps track of var type during runtime
+# class = type
+# str = Python string
+
+
+"""
+a few more observations:
+
+1. Most things in Python work, but they might not do what you expect
+2. Python has a Boolean type so True/False
+3. f() -> function call operator applied on function f 
+"""
+
+# print(type(str(int("3072"))))
+print(str(print("x")))
+
+# a = input("Enter a : ")
+# b = input("Enter b : ")
+#
+# print(type(a))
+# print(type(b))
+#
+# print(a + b)
+#
+# a = int(a)  # str() converts to a string
+# b = int(b)
+#
+# print(type(a))
+# print(type(b))
+#
+# print(a + b)
 
 """
 Main git operations:
