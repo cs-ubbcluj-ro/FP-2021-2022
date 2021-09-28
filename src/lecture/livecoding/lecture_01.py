0a9e10ac6a5f1db895d49cd31e23e57d07a2e18c
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-09-28 08:09:50 +0300

    Don't forget to commit & push your changes!

--- src/lecture/livecoding/lecture_01.py
+++ src/lecture/livecoding/lecture_01.py
@@ -3,3 +3,9 @@ print("Hello World!")
 value = input("Enter a value: ")
 
 print(value)
+
+"""
+Main git operations:
+    commit - tells git to "remember" the changes that we've just made (they are stored locally)
+      push - tells git to syncronize the remote repo (changes show up on github.com)
+"""
