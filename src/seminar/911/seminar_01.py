e0e2beabc072812806ac85e27d1b0b1f55548480
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-09-29 14:56:05 +0300

    Committing from PyCharm...

--- src/seminar/911/seminar_01.py
+++ src/seminar/911/seminar_01.py
@@ -1,5 +1,20 @@
 """
 FP seminar 1 problems & solutions
+
+git
+    - system & software for source code management (provides commit, push and many other operations)
+    - has a command line tool for almost all OSs
+
+GitHub
+    - platform for working with source code that is managed using git
+    - provides in the cloud git repositories
+
+GitHubDesktop
+    - desktop application that makes working with git & GitHub easy :)
+
+PyCharm
+    - Python development environment
+    - Provides tools to work with git (linked to GitHub or any other git repo)
 """
 
 """
@@ -7,6 +22,17 @@ FP seminar 1 problems & solutions
 Question – What happens if we enter a non-integer number, or alphanumeric characters?
 """
 
+
+# Here we declared the function (we don't call it)
+def one_is_ten(first, second):
+    # Assignment (or 'binding') transfers both the TYPE and the VALUE of the variable (right to left side)
+    return first == 10 or second == 10 or (first + second == 10)
+
+
+a = int(input("Enter a"))
+b = int(input('Enter b'))
+
+print(one_is_ten(a, b))
 
 """
 2. Write a Python program which iterates the integers from 1 to 50. 
@@ -16,11 +42,9 @@ For numbers which are multiples of both three and five print "FizzBuzz".
 FizzBuzz over the top: https://www.tomdalling.com/blog/software-design/fizzbuzz-in-too-much-detail/
 """
 
-
 """
 3. Calculate the first n terms of the Fibonacci sequence
 """
-
 
 """
 3. Write a Python program to calculate body mass index.
@@ -28,14 +52,12 @@ Question - How do we validate the code above for user input?
 ++ Let's write the specification for it
 """
 
-
 """
 4. Given a non-empty string like "Code" return a string like "CCoCodCode"
     stringSplosion('Code') → 'CCoCodCode'
     stringSplosion('abc') → 'aababc'
     stringSplosion('ab') → 'aab'
 """
-
 
 """
 5. Given 2 strings, a and b, return the number of the positions where they contain the same length 2
@@ -46,11 +68,9 @@ same place in both strings.
     stringMatch('abc', 'axc') → 0
 """
 
-
 """
 6. Write a Python program to remove all duplicate elements from a given array and returns a new array.
 """
-
 
 """
 7. Return the sum of the numbers in a list, returning 0 for an empty list. Except the number 13 is very
