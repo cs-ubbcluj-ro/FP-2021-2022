f89e92c4bb71bbfc86d77b945cb42a8c220867d5
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-10-04 12:05:28 +0300

    Implemented show all students, working on adding a student...

--- src/seminar/911/seminar_02.py
+++ src/seminar/911/seminar_02.py
@@ -33,3 +33,93 @@ The application will have a menu-driven user interface and will provide the foll
         - We can develop this program in a feature-driven manner by going through functionalities
         - We don't use global variables!
 """
+
+"""
+    Non-UI functions are here
+"""
+STUDENT_ID = 0
+STUDENT_NAME = 1
+STUDENT_GRADE = 2
+
+
+# How to represent a student
+# tuple: ('1123', 'Pop Andrei', 9) -> 1123 is the id, 9 is the grade student[0]
+# dict: {'id' : '1123', 'name' : 'Pop Andrei', 'grade': 9} # student['id']
+# code for 912/917 also has representation examples (list, dict)
+
+def create_student(student_id, student_name, student_grade):
+    """
+    Create a new student
+    :param student_id: Id (must not be empty)
+    :param student_name: Name (name is length at least 3)
+    :param student_grade: Grade (int between 1 and 10)
+    :return: Student, or None if student could not be created
+    """
+    if len(student_id) < 1 or len(student_name) < 3 or student_grade < 1 or student_grade > 10:
+        return None
+    return student_id, student_name, student_grade
+
+
+# use getters / setters to access / modify student fields
+def get_id(student):
+    return student[STUDENT_ID]
+
+
+def get_name(student):
+    return student[STUDENT_NAME]
+
+
+def get_grade(student):
+    return student[STUDENT_GRADE]
+
+
+# Non-student specific code
+def generate_students():
+    return [create_student('116', "Bodnar Andreea", 8), create_student('117', "Pop Marius", 9),
+            create_student('118', "Lazar Lucian", 10), create_student('119', "Astalus Marcel", 6)]
+
+
+"""
+    UI functions go here
+"""
+
+
+def add_student_ui():
+    pass
+
+
+def show_all_students(student_list):
+    # First we calculate the longest student name
+    just_limit = -1
+    for student in student_list:
+        just_limit = max(just_limit, len(get_name(student)))
+
+    for student in student_list:
+        # Student names are justified by the longest one
+        print(get_id(student), get_name(student).rjust(just_limit + 1), str(get_grade(student)).rjust(2))
+
+
+def print_menu():
+    print("1. Add student")
+    print("3. Show all students")
+    print("5. Exit")
+
+
+def start():
+    student_list = generate_students()
+
+    while True:
+        print_menu()
+        option = input("Enter option =")
+
+        if option == '1':
+            add_student_ui()
+        elif option == '3':
+            show_all_students(student_list)
+        elif option == '5':
+            return
+        else:
+            print("Invalid option!")
+
+
+start()
