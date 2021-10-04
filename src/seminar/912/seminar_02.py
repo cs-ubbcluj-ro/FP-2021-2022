46cf2d2586ca16a2e10e4ababaacd6dcd0f05865
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-10-04 10:55:12 +0300

    Implemented show tangent circles

--- src/seminar/912/seminar_02.py
+++ src/seminar/912/seminar_02.py
@@ -18,6 +18,8 @@ The application will have a menu-driven user interface and will provide the foll
     4. Show circles that intersect a given one
         - select a circle from the list of existing circles
         - print those which intersect it
+            1. two circles are tangent !?
+            2. two circles have a common area !?
         (bonus: sort printed circles by descending order of radius)
 
     5. exit
@@ -35,6 +37,7 @@ The application will have a menu-driven user interface and will provide the foll
         - We can develop this program in a feature-driven manner by going through functionalities
         - We don't use global variables!
 """
+import math
 
 """
     Non-UI functions 
@@ -43,7 +46,8 @@ The application will have a menu-driven user interface and will provide the foll
 
 
 # Circle functions go here
-# {'x' : 1,'y' : 2, 'rad' : 3} is circle with center at (1,2) and radius 3
+# dict: {'x' : 1,'y' : 2, 'rad' : 3} is circle with center at (1,2) and radius 3
+# list: [1,2,3] is circle with center at (1,2) and radius 3 (represented using a list)
 # We want to hide the circle's representation from the outside world (hide complexity)
 
 
@@ -59,17 +63,62 @@ def create_circle(x, y, radius):
     #     return None
     if radius < 1:
         return None  # NoneType, means the absence of a value
-    return {'x': x, 'y': y, 'rad': radius}
+    return [x, y, radius]
+    # return {'x': x, 'y': y, 'rad': radius}
 
 
 # Write getters/setters for circle center and radius
 def get_center(circle):
-    return circle['x'], circle['y']
+    return circle[0], circle[1]
+    # return circle['x'], circle['y']
 
 
 def get_radius(circle):
-    return circle['rad']
+    return circle[2]
+    # return circle['rad']
 
+
+def build_tangent_circles(circle_list, circle):
+    """
+    Return a list of the circles tangent with the given one
+    :param circle_list: The list of all circles
+    :param circle: Our circle :)
+    :return: A list of all circles that are tangent with the given one
+    """
+    result = []
+    for circ in circle_list:
+        if circles_are_tangent(circ, circle):
+            result.append(circ)
+    return result
+
+
+def circles_are_tangent(circle_1, circle_2):
+    """
+    Determines whether the two circles are tangent
+    :param circle_1: First
+    :param circle_2: Second circle
+    :return: True if they are tangent, False otherwise
+    """
+    c1 = get_center(circle_1)
+    c2 = get_center(circle_2)
+
+    r1 = get_radius(circle_1)
+    r2 = get_radius(circle_2)
+
+    center_distance = math.sqrt((c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2)
+    return center_distance == r1 + r2
+
+
+# Test function is named test_<function that is tested>
+def test_circles_are_tangent():
+    # assert crashes if the expression is False
+    assert circles_are_tangent(create_circle(0, 0, 1), create_circle(2, 0, 1)) == True
+    assert circles_are_tangent(create_circle(0, 0, 1), create_circle(1, 0, 1)) == False
+    # One circle contained in the other, we don't consider them tangent for simplicity
+    assert circles_are_tangent(create_circle(0, 0, 5), create_circle(4, 0, 1)) == False
+
+
+# test_circles_are_tangent()
 
 # Non-circle functions
 def add_circle(circle_list, circle):
@@ -86,11 +135,37 @@ def add_circle(circle_list, circle):
     return True
 
 
+def init_circles():
+    """
+    Create a few circles to have at program startup
+    :return: A list of circles
+    """
+    return [create_circle(1, 1, 1), create_circle(1, 2, 3), create_circle(2, 1, 7), create_circle(3, 3, 2)]
+
+
 """
     UI functions are here
         - program talks to the user via print/input statements 
 """
 
+
+def show_circles(circle_list):
+    index = 1
+    for circle in circle_list:
+        print(str(index) + "/ center at " + str(get_center(circle)) + " radius of " + str(get_radius(circle)))
+        index += 1
+
+
+def show_circles_intersect_ui(circle_list):
+    show_circles(circle_list)
+
+    # TODO Crash if entered values cannot be converted to int
+    # TODO Validate index!
+    index = int(input("Select circle: "))
+
+    circle = circle_list[index - 1]
+    tangent_circles = build_tangent_circles(circle_list, circle)
+    show_circles(tangent_circles)
 
 def add_circle_ui(circle_list):
     # How do we represent a circle? -> each circle is represented using a dict
@@ -111,11 +186,13 @@ def add_circle_ui(circle_list):
 
 def print_menu():
     print("1. Add circle")
+    print("3. Show circles")
+    print("4. Show circles that intersect a given one")
     print("5. Exit")
 
 
 def start():
-    circle_list = []
+    circle_list = init_circles()
 
     while True:
         print_menu()
@@ -123,6 +200,10 @@ def start():
 
         if option == '1':
             add_circle_ui(circle_list)
+        elif option == '3':
+            show_circles(circle_list)
+        elif option == '4':
+            show_circles_intersect_ui(circle_list)
         elif option == '5':
             # Exit the closest loop
             break
