a87212de9173f9c16dffe73005569fc350164a2b
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-10-05 07:45:18 +0300

    Program reqs for rational calc

---
+++ src/lecture/livecoding/lecture_02.py
@@ -0,0 +1,27 @@
+"""
+Create a calculator program for rational numbers with the following functionalities:
+    + add a rational number to the calculator
+    u undo the last operation (store the previous result!? use a stack!?)
+    x exit
+"""
+
+"""
+    Functions
+        - they have a specification (what a function does)
+        - each function should do one single
+            => Single Responsibility Principle (good programming practice)
+"""
+
+
+"""
+    Non-UI functions
+        - there will be no print/input calls here
+        - these functions will not call other functions that use print/input
+        - these functions communicate via parameters & return type and value
+"""
+
+"""
+    UI functions
+        - "talk" to the program's user
+        - rely on the non-UI functions to solve the requirements
+"""
