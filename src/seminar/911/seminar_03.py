9367927d531b690fc0e88ed4347848b2e34c8b7c
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-10-09 13:23:07 +0300

    Starter code for seminar 03

---
+++ src/seminar/911/seminar_03.py
@@ -0,0 +1,11 @@
+"""
+Seminar 3
+    -> implement command-based user interface
+        add 100, Pop Marian, 5
+        delete 2,3, 4, 6 # delete students having one of those id's
+        delete 2 - 10 # delete all students where 2 <= id <= 10
+        list # display the list of students
+        exit
+    -> add Exceptions
+    -> add unit tests
+"""
---
+++ src/seminar/912/seminar_03.py
@@ -0,0 +1,10 @@
+"""
+Seminar 3
+    -> implement command-based user interface
+        add 0,0,1
+        delete 0,0; 1,1 # delete circles at center (0,0) and (1,1)
+        list # display the list of circles
+        exit
+    -> add Exceptions
+    -> add unit tests
+"""
