d7f1607d62b016f3131836b255d284dea5e56d53
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-11-02 07:53:11 +0200

    Bakery problem statement

--- src/lecture/livecoding/lecture_06_08/start.py
+++ src/lecture/livecoding/lecture_06_08/start.py
@@ -9,3 +9,33 @@ What does this entail?
     e. Show used ingredients, sorted in descending order of amount used.
 """
 
+"""
+Motivation / Recap
+
+~~~
+Managing complexity is a big deal in software engineering (SE)
+         complexity = what can humans understand
+~~~
+1. Divide the program into functions (parameters/return/exceptions)
+2. Divide the program into modules (+ function)
+3. Add our own data types (classes) 
+    - we want to represent data/operations our own way
+4. We try to combine 1. + 2. + 3. into a well-used design pattern => layered architecture
+
+What layers have we already used?
+    -> ui (user interaction)
+    -> services (functionality)
+    -> repository (data store)
+    -> domain (our program's data types)
+    
+Calls between layers
+    ui -> services -> repository -> domain
+    ui -> domain
+
+Future work
+    services   -> statistics / filters / undo/redo 
+    repository -> manage data storage (in-memory -> text-files -> binary files -> SQL/noSQL (opt) )
+
+Modules -> responsible for one thing only (SRP)
+        -> independent & interchangeable (text-file storage vs. SQL storage)
+"""
