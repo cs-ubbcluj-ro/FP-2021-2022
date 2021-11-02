8c2d5e430a8950905130d212fbf3860bc43e7983
Arthur Molnar <arthur@cs.ubbcluj.ro>
2021-11-02 07:39:18 +0200

    Bakery problem statement

---
+++ src/lecture/livecoding/lecture_06_08/start.py
@@ -0,0 +1,11 @@
+"""
+The Happy Bakery is a family-run business that produces and sells bakery and confectionary products.
+The bakery turns ingredients into delicious products using defined recipes.
+They need you, yes you! to help them manage their daily workflow.
+What does this entail?
+    a. Manage available ingredients, recipes and products that the bakery knows to make
+    b. Record a production run. This transforms existing ingredients into product, according to the recipe
+    c. Keep the stock for each ingredient and product
+    e. Show used ingredients, sorted in descending order of amount used.
+"""
+
