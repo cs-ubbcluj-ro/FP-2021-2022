197501a0bce7557bddedeaf15d20613005b2d5f2
Arthur Molnar <arthur@cs.ubbcluj.ro>
2022-01-01 17:54:41 +0200

    Divide & conquer, backtracking

---
+++ src/seminar/group_911/seminar_13.py
@@ -0,0 +1,83 @@
+"""
+    Divide & Conquer
+"""
+
+"""
+1. Find the smallest number in a list (chip & conquer, divide in halves, recursive vs non-recursive)
+    a. Chip & conquer, recursive
+    b. Divide in halves, non-recursive
+    c. Divide in halves, recursive
+"""
+
+"""
+2. Calculate the r-th root of a given number x with a given precision p
+"""
+
+"""
+3. Calculate the maximum subarray sum (subarray = elements having continuous indices)
+    a. Naive implementation
+    b. Divide & conquer implementation
+    
+    e.g.
+    for data = [-2, -5, 6, -2, -3, 1, 5, -6], maximum subarray sum is 7.
+"""
+
+"""
+    Backtracking
+"""
+
+"""
+4. Recursive implementation for permutations
+"""
+
+
+def consistent(x):
+    """
+    Determines whether the current partial array can lead to a solution
+    """
+    return len(set(x)) == len(x)
+
+
+def solution(x, n):
+    """
+    Determines whether we have a solution
+    """
+    return len(x) == n
+
+
+def solution_found(x):
+    """
+    What to do when a solution is found
+    """
+    print("Solution: ", x)
+
+
+def bkt_rec(x, n):
+    """
+    Backtracking algorithm for permutations problem, recursive implementation
+    """
+    x.append(0)
+    for i in range(0, n):
+        x[len(x) - 1] = i
+        if consistent(x):
+            if solution(x, n):
+                solution_found(x)
+            else:
+                bkt_rec(x[:], n)
+
+
+# print(bkt_rec([], 4))
+
+"""
+5. Change the code for generating the permutation above to work for the n-Queen problem
+"""
+
+"""
+A Latin square is an n × n square filled with n different symbols, each occurring exactly once in each row and exactly 
+once in each column
+
+6. Generate all the N x N Latin squares for a given number N. 
+
+7. Generate all reduced N x N Latin squares for a given number N. In a reduced Latin square, the elements of the first 
+row and column are sorted.
+"""
